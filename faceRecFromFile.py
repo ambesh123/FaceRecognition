7d0ada306041a26e5ffb86b0910bc4ad3ca5eb00
Ambesh Tiwari <35375583+ambesh123@users.noreply.github.com>
2021-11-11 18:39:16 +0530

    Added project files via upload

---
+++ faceRecFromFile.py
@@ -0,0 +1,147 @@
+import face_recognition
+import cv2
+import numpy as np
+from gtts import gTTS
+import os
+import threading
+
+# Function for text to speech
+def say_this(text):
+    myobj = gTTS(text=text, lang='en', slow=False)  
+    myobj.save("temp.mp3") 
+    os.system("mpg123 temp.mp3") 
+
+# Add a person to find list
+found_list = []
+already_found = {'Unknown' : True}
+
+def addPerson(name):
+    if not name in already_found:
+        already_found[name] = True
+        found_list.append(name)
+        say_this('found '+name)
+
+# Has correct image extension
+def has_image_extension (file_name):
+    l1 = len(file_name)
+    for extn in expected_image_extensions:
+        l2 = len(extn)
+        i = l2 - 1
+        while i >= 0:
+            if extn[i] != file_name[l1-l2+i]:
+                break
+            i -= 1
+        if i == -1:
+            return True
+    return False
+
+def get_name_from_file(file_name):
+    l = len(file_name) - 1
+    while file_name[l] != '.':
+        l -= 1
+    return file_name[0:l]
+
+# This is a demo of running face recognition on live video from your webcam.
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Array of expected image extension
+expected_image_extensions = ['.jpg', '.png', '.jpeg']
+
+# Array of known face encodings and their names
+known_face_encodings = []
+known_face_names = []
+
+file_list = os.listdir('.')
+
+for file in file_list:
+    if has_image_extension(file):
+        person_image = face_recognition.load_image_file(file)
+        person_image_encoding = face_recognition.face_encodings(person_image)[0]
+        person_name = get_name_from_file(file)
+        known_face_names.append(person_name)
+        known_face_encodings.append(person_image_encoding)
+
+
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Add this person to find list | call in new thread to avoid pausing of video
+        name_thread = threading.Thread(target= addPerson, args= (name,))
+        name_thread.start()
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+#print name of all persons found
+for person in found_list:
+    print(person)
---
+++ tts.py
@@ -0,0 +1,5 @@
+import os
+
+# file_list = os.listdir('.')
+# for f in file_list:
+# 	print(f)
